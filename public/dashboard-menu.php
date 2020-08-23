656cf2ddcce33597c839ba45d5c13ffdbc836c4e
BERRAHAL MOHAMMED <44066991+alfennec@users.noreply.github.com>
2020-08-23 12:44:43 +0100

    finish part upload data

--- public/dashboard-menu.php
+++ public/dashboard-menu.php
@@ -74,13 +74,22 @@
         // common image file extensions
         $allowedExts = array("gif", "jpeg", "jpg", "png");
 
-        //---------------- get image file extension bac
         error_reporting(E_ERROR | E_PARSE);
         $extension_bac = end(explode(".", $_FILES["img_bac"]["name"]));
-    
+
+        error_reporting(E_ERROR | E_PARSE);
+        $extension_cin = end(explode(".", $_FILES["img_cin"]["name"]));
+
+        error_reporting(E_ERROR | E_PARSE);
+        $extension_relever = end(explode(".", $_FILES["img_relever"]["name"]));
+
+        error_reporting(E_ERROR | E_PARSE);
+        $extension_etudiant = end(explode(".", $_FILES["img_etudiant"]["name"]));
+
+        //---------------- get image file extension bac
         if($image_error > 0) 
         {
-            $error['img_cin'] = " <span class='font-12 col-red'>Vous n'avez pas insérer une images !!</span>";
+            $error['img_bac'] = " <span class='font-12 col-red'>Vous n'avez pas insérer une images !!</span>";
         } else if(!(($img_bac_type == "image/gif") ||
                 ($img_bac_type == "image/jpeg") ||
                 ($img_bac_type == "image/jpg") ||
@@ -89,13 +98,10 @@
                 ($img_bac_type == "image/pjpeg")) &&
             !(in_array($extension_bac, $allowedExts)))
             {
-                $error['img_cin'] = " <span class='font-12'>Le type d'image doit etre en jpg, jpeg, ou bien png !</span>";
+                $error['img_bac'] = " <span class='font-12'>Le type d'image doit etre en jpg, jpeg, ou bien png !</span>";
             }
 
         //---------------- get image file extension cin
-        error_reporting(E_ERROR | E_PARSE);
-        $extension_cin = end(explode(".", $_FILES["img_cin"]["name"]));
-
         if($image_error > 0) 
         {
             $error['img_cin'] = " <span class='font-12 col-red'>Vous n'avez pas insérer une images !!</span>";
@@ -109,6 +115,38 @@
             {
                 $error['img_cin'] = " <span class='font-12'>Le type d'image doit etre en jpg, jpeg, ou bien png !</span>";
             }
+
+        //---------------- get image file extension cin
+        if($image_error > 0) 
+        {
+            $error['img_relever'] = " <span class='font-12 col-red'>Vous n'avez pas insérer une images !!</span>";
+        } else if(!(($img_relever_type == "image/gif") ||
+                ($img_relever_type == "image/jpeg") ||
+                ($img_img_relever_typecin_type == "image/jpg") ||
+                ($img_relever_type == "image/x-png") ||
+                ($img_relever_type == "image/png") ||
+                ($img_relever_type == "image/pjpeg")) &&
+            !(in_array($extension_relever, $allowedExts)))
+            {
+                $error['img_relever'] = " <span class='font-12'>Le type d'image doit etre en jpg, jpeg, ou bien png !</span>";
+            }
+
+        
+        //---------------- get image file extension cin
+        if($image_error > 0) 
+        {
+            $error['img_etudiant'] = " <span class='font-12 col-red'>Vous n'avez pas insérer une images !!</span>";
+        } else if(!(($img_etudiant_type == "image/gif") ||
+                ($img_etudiant_type == "image/jpeg") ||
+                ($img_etudiant_type == "image/jpg") ||
+                ($img_etudiant_type == "image/x-png") ||
+                ($img_etudiant_type == "image/png") ||
+                ($img_etudiant_type == "image/pjpeg")) &&
+            !(in_array($extension_etudiant, $allowedExts)))
+            {
+                $error['img_etudiant'] = " <span class='font-12'>Le type d'image doit etre en jpg, jpeg, ou bien png !</span>";
+            }
+
         
         if (!empty($id_account) && 
             !empty($nom_fr) && 
@@ -133,13 +171,24 @@
                 
                 
                 // create random image file name bac
-                $string = '0123456789';
                 $file = preg_replace("/\s+/", "_", $_FILES['img_bac']['name']);
-                $function = new functions;
-                $img_bac_up = $nom_fr."_".$prenon_fr."_".$function->get_random_string($string, 4)."-".date("Y-m-d").".".$extension_bac;
-
-                // upload new image
+                $img_bac_up = $nom_fr."_".$prenon_fr."_".date("Y-m-d").".".$extension_bac;
                 $upload = move_uploaded_file($_FILES['img_bac']['tmp_name'], 'upload/bac/'.$img_bac_up);
+
+                // create random image file name cin
+                $file = preg_replace("/\s+/", "_", $_FILES['img_cin']['name']);
+                $img_cin_up = $nom_fr."_".$prenon_fr."_".date("Y-m-d").".".$extension_cin;
+                $upload = move_uploaded_file($_FILES['img_cin']['tmp_name'], 'upload/cin/'.$img_cin_up);
+
+                // create random image file name relever
+                $file = preg_replace("/\s+/", "_", $_FILES['img_relever']['name']);
+                $img_relever_up = $nom_fr."_".$prenon_fr."_".date("Y-m-d").".".$extension_relever;
+                $upload = move_uploaded_file($_FILES['img_relever']['tmp_name'], 'upload/relever/'.$img_relever_up);
+
+                // create random image file name etudiant
+                $file = preg_replace("/\s+/", "_", $_FILES['img_etudiant']['name']);
+                $img_etudiant_up = $nom_fr."_".$prenon_fr."_".date("Y-m-d").".".$extension_etudiant;
+                $upload = move_uploaded_file($_FILES['img_etudiant']['tmp_name'], 'upload/etudiant/'.$img_etudiant_up);
 
             // insert new data to menu table
             $sql_query = "INSERT INTO inscription_nv (
@@ -162,15 +211,18 @@
                 serie_bac,
                 montion_bac,
                 img_bac,
+                img_cin,
+                img_relever,
+                img_etudiant,
                 terms,
                 section
-                ) VALUES(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)";
+                ) VALUES(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)";
 
             $stmt = $connect->stmt_init();
             if($stmt->prepare($sql_query)) 
             {    
                 // Bind your variables to replace the ?s
-                $stmt->bind_param('sssssssssssssssssssss', 
+                $stmt->bind_param('ssssssssssssssssssssssss', 
                         $id_account, 
                         $nom_fr, 
                         $prenon_fr, 
@@ -190,6 +242,9 @@
                         $serie_bac,
                         $montion_bac,
                         $img_bac_up,
+                        $img_cin_up,
+                        $img_relever_up,
+                        $img_etudiant_up,
                         $terms,
                         $section
                             );
